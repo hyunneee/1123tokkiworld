bdeca71be2a9300b0c5c7ca0853da9df9493910f
hyunneee <sh_oh@u.sogang.ac.kr>
2023-11-23 14:06:47 +0900

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import path from 'path';
+
 export default defineConfig({
   build: {
     outDir: 'dist',
-    assetsInclude: ['**/*.html', '**/*.js', '**/*.css', '**/*.glb', '**/*/*.glb'],
+    assetsInclude: ['**/*.html', '**/*.js', '**/*.css', '**/*.png', '**/*.svg', '**/*.glb'],
     rollupOptions: {
       treeshake: false,
     },
   },
   resolve: {
     alias: {
-      // 'three': 'three',
-      'three': path.resolve(__dirname, 'node_modules/three'),
+      'three': 'three',
+    // 'three': path.resolve(__dirn
+      'src': path.resolve(__dirname, 'src'), // src 폴더를 짧은 별칭으로 설정
     },
   },
 });
